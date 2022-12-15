7c547923962972b2ca99e2bf567957738f32a083
bogdantro <sabertoothtri@gmail.com>
2022-12-15 20:07:27 +0200

    as

--- static/js/home.js
+++ static/js/home.js
@@ -8,3 +8,21 @@ function stepOneClose(){
 
     makeRoute.classList.remove('active');
 }
+function validateStepOne(){
+    const makeRoute = document.getElementById('makeRoute');
+    const routeCords = document.getElementById('routeCords');
+    const customCords = document.getElementById('customCords');
+    const makeRouteError = document.getElementById('makeRouteError');
+    
+    if(routeCords.value){
+        makeRoute.classList.remove('active');
+        makeRouteError.style.display = 'none';
+        customCords.style.pointerEvents = 'none';
+    }else if (customCords.value){
+        makeRoute.classList.remove('active');
+        makeRouteError.style.display = 'none';
+    }else{
+        customCords.style.pointerEvents = 'visible';
+        makeRouteError.style.display = 'block';        
+    }
+}
